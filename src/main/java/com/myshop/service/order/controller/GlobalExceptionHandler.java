fddfb938ea1456b368b8246a0e8480b26c2daad1
ertakbuga <46849393+ertakbuga@users.noreply.github.com>
2019-01-24 09:36:52 +0300

    Correction of Global Exception Handler

--- src/main/java/com/myshop/service/order/controller/GlobalExceptionHandler.java
+++ src/main/java/com/myshop/service/order/controller/GlobalExceptionHandler.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
-    /*
-    @ExceptionHandler(value = OrderServiceException.class)
-    public OrderResponse handleException(OrderServiceException e) {
+    
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public OrderResponse handleException(DataIntegrityViolationException e) {
         OrderResponse orderResponse = new OrderResponse();
         orderResponse.setStatusCode(ResponseStatus.ERROR);
         orderResponse.setMessage(e.getCause().getMessage());
         return orderResponse;
-    }*/
+    }
 }
